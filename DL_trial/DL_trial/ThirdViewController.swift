3ff81b15573381348e4ecdb4e02b964cf342f798
Youdong Zhang <youdongz@berkeley.com>
2016-04-22 14:20:53 -0700

    views?

---
+++ DL_trial/DL_trial/ThirdViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThirdViewController.swift
+//  DL_trial
+//
+//  Created by Youdong Zhang on 4/22/16.
+//  Copyright © 2016 Chya 2.0. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
