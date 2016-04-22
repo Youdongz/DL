c2ae231b110782cdb16708cb8bdf6d75589919c9
Pinxiu Gong <pinxiu@berkeley.edu>
2016-04-22 12:52:37 -0700

    Empty Start

---
+++ DL_trial/DL_trial/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DL_trial
+//
+//  Created by MacBook Pro on 4/22/16.
+//  Copyright © 2016 Chya 2.0. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
